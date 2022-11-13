3f370f1b047dde034abd5bf0ffe4ea1643eaf9d2
dvphuc96 <phuc.dovan.96@gmail.com>
2022-11-13 15:33:43 +0700

    remove warning

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import HomeTemplate from "./templates/HomeTemplate";
 import ShoeStore from "./pages/ShoesStore/ShoeShop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,7 +9,6 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="" element={<ShoeStore />}>
-          {/* <Route path="shop" element={<ShoeStore />}></Route> */}
           <Route path="*" element={<Navigate to="" />}></Route>
         </Route>
       </Routes>
